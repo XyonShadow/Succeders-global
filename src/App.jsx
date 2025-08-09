be5c07c9ed284e8ec316edd4601a6d8a0102e3ae
Michael <prncemk11@gmail.com>
2025-08-09 15:31:12 +0100

    Add basic Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './styles/App.css';
+import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <Navbar />
         
         <Routes>
-          <Route path="/" element={<div className="p-4">Home Page</div>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Welcome to Succeeders Global</h1>
+    </>
+  );
+};
+
+export default Home;
