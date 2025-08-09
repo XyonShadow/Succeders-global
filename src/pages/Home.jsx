f26788a759141e10e871c791aa8745193ac6455e
Michael <prncemk11@gmail.com>
2025-08-09 16:04:38 +0100

    Add WelcomeMessage component and include it in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import WelcomeMessage from '../components/WelcomeMessage.jsx';
 
 const Home = () => {
   return (
     <>
       < Hero/>
+      <WelcomeMessage />
     </>
   );
 };
