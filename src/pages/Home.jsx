13c5ca527b1bb013494d25ccfb54a60d70065305
Michael <prncemk11@gmail.com>
2025-08-09 17:21:02 +0100

    Add Events component and include it in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import Hero from '../components/Hero';
 import WelcomeMessage from '../components/WelcomeMessage.jsx';
 import ServiceTimes from '../components/ServiceTimes.jsx';
 import Ministries from '../components/Ministries.jsx';
+import Events from '../components/Events.jsx';
 
 const Home = () => {
   return (
     <>
-      < Hero/>
+      <Hero />
       <WelcomeMessage />
       <ServiceTimes />
       <Ministries />
+      <Events />
     </>
   );
 };
