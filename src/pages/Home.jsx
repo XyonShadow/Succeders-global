40612f16ba979f8e4d56e2fb16971782f4791d9d
Michael <prncemk11@gmail.com>
2025-08-09 16:17:36 +0100

    Add ServiceTimes component and include it in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import WelcomeMessage from '../components/WelcomeMessage.jsx';
+import ServiceTimes from '../components/ServiceTimes.jsx';
 
 const Home = () => {
   return (
     <>
       < Hero/>
       <WelcomeMessage />
+      <ServiceTimes />
     </>
   );
 };
